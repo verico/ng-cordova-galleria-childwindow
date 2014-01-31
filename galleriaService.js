b0c25f2b6dd74489cdda1ada76d5362e7297471c
a_waal@hotmail.com <rinins6>
2014-01-31 14:15:26 +0100

    Initial commit

---
+++ galleriaService.js
@@ -0,0 +1,25 @@
+angular.module('com.verico.ng-cordova-galleria-childwindow', []).
+    service('ngCordovaGalleriaChildwindow', function() {
+
+        var openChildWindow = function(windowUrl, images){
+
+            var ref = window.open(windowUrl, '_blank', 'EnableViewPortScale=yes,location=no');
+
+            var loadstop = function() {
+                ref.executeScript({ code: 'init(\'' +JSON.stringify(images) + '\');' });
+            };
+            var exit = function() {
+                ref.removeEventListener('loadstop', loadstop);
+                ref.removeEventListener('exit', exit);
+            };
+            ref.addEventListener('loadstop', loadstop);
+            ref.addEventListener('exit', exit);
+
+
+        };
+
+
+        return {
+            open : openChildWindow
+        };
+    });
